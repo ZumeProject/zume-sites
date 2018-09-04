0e31c91423640376ae760921aa00930f9c8d5474
Chris Chasm <chris@chasm.solutions>
2018-09-04 14:09:42 -0600

    Added login styles

--- zume-sites.php
+++ zume-sites.php
@@ -18,8 +18,21 @@ add_action( 'wpmu_new_blog', 'zume_new_blog_force_dt_theme', 10, 6 );
 function zume_multisite_disable_header() {
     ?>
     <style type="text/css">
-
+        div#signup-content {
+            margin: 15% auto;
+            width: 30%;
+            border: 1px solid #323A68;
+        }
+        .mu_register h2 {display:none;}
+        p .submit {
+            padding: 15px;
+        }
     </style>
+    <script>
+        jQuery(document).ready(function(){
+
+        })
+    </script>
     <?php
 }
 add_action( 'signup_header', 'zume_multisite_disable_header' );
